474a17d5be5ba4e93d85e411a407e795f7043504
hiroki-saito <mail@hsaito.com>
2022-08-21 11:53:36 +0900

    fix 自動生成されたコードをstanに対応

--- app/Http/Livewire/Counter.php
+++ app/Http/Livewire/Counter.php
@@ -2,18 +2,19 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\View\View;
 use Livewire\Component;
 
 class Counter extends Component
 {
-    public $count = 0;
+    public int $count = 0;
 
-    public function increment()
+    public function increment(): void
     {
         $this->count++;
     }
 
-    public function render()
+    public function render(): View
     {
         return view('livewire.counter');
     }
